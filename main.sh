403797f086cbb3b1ebfd070faf04418e50c298cc
Rahul <rahulma@thougthtworks.com>
2023-06-04 17:44:13 +0530

    Allow users to disable showing sonarqube logs on console

--- main.sh
+++ main.sh
@@ -6,12 +6,19 @@ host=$(echo "$file" | yq ".sonarqube.host")
 project_name=$(echo "$file" | yq ".sonarqube.project_name")
 token=$(echo "$file" | yq ".sonarqube.token")
 project_path=$(echo "$file" | yq ".sonarqube.project_path")
+show_logs=$(echo "$file" | yq ".sonarqube.show_logs")
+
+LOG_REDIRECTION="/dev/tty"
+if [ $show_logs == "false" ]; then
+  LOG_REDIRECTION="/dev/null"
+fi
 
 sonar-scanner \
   -Dsonar.projectKey=$project_name \
   -Dsonar.sources=$project_path \
   -Dsonar.host.url=$host \
-  -Dsonar.login=$token
+  -Dsonar.login=$token > $LOG_REDIRECTION
+
 
 
 echo "security checks ran successfully !!!"
