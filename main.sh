909458de77336857a653d15194c5d0a9d5d12e71
Rahul <rahulma@thougthtworks.com>
2023-06-04 15:48:33 +0530

    Get correct scanner cli & update jdk version to from 1.8 to 11

--- main.sh
+++ main.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
 
+file=$(cat /tmp/config.yaml)
+
+host=$(echo "$file" | yq ".sonarqube.host")
+project_name=$(echo "$file" | yq ".sonarqube.project_name")
+token=$(echo "$file" | yq ".sonarqube.token")
+project_path=$(echo "$file" | yq ".sonarqube.project_path")
+
+sonar-scanner \
+  -Dsonar.projectKey=$project_name \
+  -Dsonar.sources=$project_path \
+  -Dsonar.host.url=$host \
+  -Dsonar.login=$token
+
 
 echo "security checks ran successfully !!!"
