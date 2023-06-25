845c8d6f0f5093f2483e984edf5dd2256b753539
Rahul <rahulma@thougthtworks.com>
2023-06-25 13:48:52 +0530

    remove unnecessary paramters from sonarqube config

--- main.sh
+++ main.sh
@@ -3,8 +3,6 @@
 file=$(cat /tmp/config.yaml)
 
 host=$(echo "$file" | yq ".sonarqube.host")
-project_name=$(echo "$file" | yq ".sonarqube.project_name")
-token=$(echo "$file" | yq ".sonarqube.token")
 project_path=$(echo "$file" | yq ".sonarqube.project_path")
 show_logs=$(echo "$file" | yq ".sonarqube.show_logs")
 
@@ -14,11 +12,9 @@ if [ $show_logs == "false" ]; then
 fi
 
 sonar-scanner \
-  -Dsonar.projectKey=$project_name \
-  -Dsonar.sources=$project_path \
+  -Dproject.settings=$project_path/sonar-project.properties \
   -Dsonar.host.url=$host \
-  -Dsonar.login=$token > $LOG_REDIRECTION
-
+  -Dsonar.sources=$project_path > $LOG_REDIRECTION
 
 
 echo "security checks ran successfully !!!"
