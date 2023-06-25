42af9aa8a4ef7aaf768dbd55564e5c142de0eb13
Rahul <rahulma@thougthtworks.com>
2023-06-25 13:54:42 +0530

    Allow user to specify a different project properties file

--- main.sh
+++ main.sh
@@ -4,15 +4,21 @@ file=$(cat /tmp/config.yaml)
 
 host=$(echo "$file" | yq ".sonarqube.host")
 project_path=$(echo "$file" | yq ".sonarqube.project_path")
+input_properties_file_name=$(echo "$file" | yq ".sonarqube.project_properties_file")
 show_logs=$(echo "$file" | yq ".sonarqube.show_logs")
 
 LOG_REDIRECTION="/dev/tty"
-if [ $show_logs == "false" ]; then
+if [ $show_logs != "" ]; then
   LOG_REDIRECTION="/dev/null"
 fi
 
+PROJECT_PROPERTIES_FILE="sonar-project.properties"
+if [ $input_properties_file_name != "" ]; then
+  PROJECT_PROPERTIES_FILE="$input_properties_file_name"
+fi
+
 sonar-scanner \
-  -Dproject.settings=$project_path/sonar-project.properties \
+  -Dproject.settings=$project_path/$PROJECT_PROPERTIES_FILE \
   -Dsonar.host.url=$host \
   -Dsonar.sources=$project_path > $LOG_REDIRECTION
 
